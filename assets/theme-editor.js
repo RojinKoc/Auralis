679fad49f66fdad7bddb45f56b1522e52d6e28ca
Rojin Koç <rojinin.koc@gmail.com>
2024-08-29 17:22:34 +0300

    descAccordionJS

--- assets/theme-editor.js
+++ assets/theme-editor.js
@@ -68,4 +68,19 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
+document.addEventListener("DOMContentLoaded", function () {
+  var accordions = document.getElementsByClassName("accordion");
+
+  for (var i = 0; i < accordions.length; i++) {
+    accordions[i].addEventListener("click", function () {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      if (content.style.display === "block") {
+        content.style.display = "none";
+      } else {
+        content.style.display = "block";
+      }
+    });
+  }
+});
 
