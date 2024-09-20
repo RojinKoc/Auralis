078022569f7af39a9ddb20e51ad3d58ee22710cc
Rojin Koç <rojinin.koc@gmail.com>
2024-09-20 13:27:25 +0300

    infinite

--- assets/media-gallery.js
+++ assets/media-gallery.js
@@ -115,3 +115,14 @@ if (!customElements.get('media-gallery')) {
     }
   );
 }
+onSlideChanged(event) {
+  const thumbnail = this.elements.thumbnails.querySelector(
+    `[data-target="${event.detail.currentElement.dataset.mediaId}"]`
+  );
+  this.setActiveThumbnail(thumbnail);
+
+  // Eğer son görsele ulaştıysanız, başa dönmek için bu kısmı ekleyin:
+  if (event.detail.currentElement === this.elements.viewer.lastElementChild) {
+    this.elements.viewer.firstElementChild.scrollIntoView({ behavior: 'smooth' });
+  }
+}
